e9eb9d51036dbc3b6c3b5d9194be0d2d402cc295
Tomáš Matoušek <tmat@users.noreply.github.com>
2017-07-21 14:06:23 -0700

    Update VSIBuild.cmd

--- VSIBuild.cmd
+++ VSIBuild.cmd
@@ -1,3 +1,21 @@
 @echo off
+
+if "%1" == "-configuration" if "%3" == "-prepareMachine" (
+  SET EXE_DIR=%~dp0artifacts\%2\tmp
+  SET LOG_DIR=%~dp0artifacts\%2\log
+  SET EXE=%EXE_DIR%\dotnet-dev-win-x64.1.0.4.exe
+  SET LOG=%LOG_DIR%\cli_install.log
+
+  mkdir "%EXE_DIR%"
+  mkdir "%LOG_DIR%"
+  
+  echo Downloading dotnet ...
+  powershell -NoProfile -ExecutionPolicy Bypass -Command "Invoke-WebRequest 'https://download.microsoft.com/download/B/9/F/B9F1AF57-C14A-4670-9973-CDF47209B5BF/dotnet-dev-win-x64.1.0.4.exe' -OutFile '%EXE%'"
+  
+  echo Installing %EXE% ...
+  "%EXE%" /install /quiet /norestart /log "%LOG%"
+)
+
 powershell -ExecutionPolicy ByPass %~dp0build\Build.ps1 -restore -deployDeps -build -deploy -integrationTest -ci %*
-exit /b %ErrorLevel%
+exit /b %ERRORLEVEL%
+
